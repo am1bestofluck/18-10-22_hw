953db586e27bcb8dc780d1025353bd538db3c26e
am_6733 <anton6733@gmail.com>
2022-10-24 01:04:59 +0300

    blnt

---
+++ task_c-sharp/t1.cs
@@ -0,0 +1,22 @@
+using static System.Console;
+class input_flow
+{
+    public static void todo()
+    {
+        WriteLine("Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.");
+    }
+    protected dynamic routine()
+    {
+        bool is_valid_input=false;
+        List<int> answers= new List<int>();
+        while (true)
+        {
+            
+        }
+
+    }
+    public input_flow(){
+        this.routine();
+
+    }
+}
