9c835cc789b078c61cd3ba4ccdb14ca64fd35769
am_6733 <anton6733@gmail.com>
2022-10-25 00:10:08 +0300

    some 2nd stuff

---
+++ task_c-sharp/t2.cs
@@ -0,0 +1,16 @@
+class input_flow_limited:input_flow
+{
+    new protected dynamic routine()
+    {
+        Dictionary<string,int> output = new Dictionary<string, int>();
+        return output;
+    }
+}
+class linear_equation{
+    Dictionary<string,int> values= new Dictionary<string, int>();
+    public linear_equation(Dictionary<string,int> keys_inc)
+    {
+        
+    }
+
+}
